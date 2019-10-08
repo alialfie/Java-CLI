d513d4f228ebda8826a4cd51656542889e1eb3b5
alialfie <alisamy27@gmail.com>
2019-10-08 22:30:28 +0200

    added parser and terminal classes

---
+++ src/Parser.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class Parser {
+	String cmd;
+	ArrayList args;
+	
+	public Parser() {
+		cmd = "";
+		args = new ArrayList<String>();
+	}
+
+	public boolean parse(String input) {
+		int length = input.length(), i = 0;
+		while(input.charAt(i) != ' ' && i < length) {
+			cmd += input.charAt(i);
+			i++;
+		}
+		
+		i++;	//skip the space
+		
+		while(i < length) {
+			String arg = "";
+			while(input.charAt(i) != ' ') {
+				arg += input.charAt(i);
+				i++;
+			}
+		}
+		
+		
+		return true;
+	}
+
+	public String getCmd() {
+		return cmd;
+	}
+
+	public ArrayList getArgs() {
+		return args;
+	}
+	
+	
+}
