3635f145fd0defd19219161d62e7e9afa2e60b5e
alialfie <alisamy27@gmail.com>
2019-10-09 17:12:37 +0200

    finished parser

---
+++ src/CLI.java
@@ -0,0 +1,8 @@
+
+public class CLI {
+	public static void main(String[] args) {
+		Parser parser = new Parser();
+		parser.parse("cd 0 ");
+	}
+	
+}
--- src/Parser.java
+++ src/Parser.java
@@ -1,33 +1,61 @@
-import java.util.ArrayList;
+import java.util.*;
+import javafx.util.Pair;
 
 public class Parser {
 	String cmd;
 	ArrayList args;
+	ArrayList validCommands;
 	
 	public Parser() {
 		cmd = "";
 		args = new ArrayList<String>();
+		
+		validCommands = new ArrayList<Pair<String, Integer>>() {
+			{
+				add(new Pair("cd", 0));
+				add(new Pair("cp", 2));
+			}
+		};
 	}
 
 	public boolean parse(String input) {
 		int length = input.length(), i = 0;
-		while(input.charAt(i) != ' ' && i < length) {
+		while(input.charAt(i) != ' ' && i < length) {	//gets the command line
 			cmd += input.charAt(i);
 			i++;
 		}
 		
 		i++;	//skip the space
 		
-		while(i < length) {
+		while(i < length) {		//gets the arguments
 			String arg = "";
-			while(input.charAt(i) != ' ') {
+			while(input.charAt(i) != ' ' && i < length) {
 				arg += input.charAt(i);
 				i++;
 			}
+			args.add(arg);
+			i++;	//skip the space
 		}
 		
+		System.out.println(validateCommand());
 		
-		return true;
+		return validateCommand();
+	}
+	
+	private boolean validateCommand() {
+		Iterator<Pair<String, Integer>> iterator = validCommands.iterator();
+		Pair<String, Integer> current;
+		while(iterator.hasNext()) {
+			current = iterator.next();
+			if(current.getKey().equals(cmd)) {
+				if(args.size() >= (Integer)current.getValue()) {
+					return true;
+				}else {
+					return false;
+				}
+			}
+		}
+		return false;
 	}
 
 	public String getCmd() {
