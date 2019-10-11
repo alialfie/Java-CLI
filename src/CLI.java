28136c2f6ae052f32b195f841901739d06d2a6e2
y0ussefm0hamed <y0y01311999@gmail.com>
2019-10-11 18:07:30 +0200

    added cd command

--- src/CLI.java
+++ src/CLI.java
@@ -7,7 +7,7 @@ public class CLI {
 		//parser.parse("clear");
 		Scanner kb = new Scanner(System.in);
 		
-		
+		T.cd("E:/M.U.G.E.N");
 		String comnd = kb.next();
 		if (comnd.equalsIgnoreCase("date")) {
 			T.date();
--- src/Terminal.java
+++ src/Terminal.java
@@ -10,6 +10,14 @@ import java.util.stream.Stream;
 
 public class Terminal {
 
+	String defaultDir;
+	String path;
+
+	public Terminal() {
+		defaultDir = "C:/";
+		path = defaultDir;
+	}
+	
 	public void date() {
 		Date date = new Date();
 		SimpleDateFormat aDate = new SimpleDateFormat("MM-dd-yyyy HH:mm:ss");
@@ -17,10 +25,34 @@ public class Terminal {
 		System.out.println(aDate.format(date));
 		}
 	
-	public void pwd() {
-		String currentDir = System.getProperty("user.dir");
-		System.out.println(currentDir);
+	//public void pwd() {
+	//	String currentDir = System.getProperty("user.dir");
+	//	System.out.println(currentDir);
+	//}
+	
+	public void cd(String path) {
+		String backupPath = this.path;
+		if(path.equals("")) {
+			this.path = this.defaultDir;
+		}else if(path.charAt(1) == ':') {
+			this.path = path;
+		}else if(path.charAt(0) != '/') {
+			this.path = this.path + path;
+		}else if(path.charAt(0) == '/') {
+			this.path = "C:" + path;
+		}
+		
+		if(this.path.charAt(this.path.length()-1) != '/') {
+			this.path += "/";
+		}
+		
+		File f = new File(this.path);
+		if(!f.isDirectory()) {
+			System.out.println("invalid directory");
+			this.path = backupPath;
+		}
 	}
+	
 	
 	public void mkdir(String dirName) {
 		boolean exists =  new File(dirName).mkdir();    
@@ -31,5 +63,10 @@ public class Terminal {
 	
 	public void ls() {
 		
+	}
+	
+
+	public void pwd() {
+		System.out.println(path);
 	}
 }
