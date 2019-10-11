44151bd85a7e93296d8949e6f2d679600739c9e1
y0ussefm0hamed <y0y01311999@gmail.com>
2019-10-11 17:38:20 +0200

    added mkdir, date and pwd functions and tested them.

--- src/CLI.java
+++ src/CLI.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
 
 public class CLI {
 	public static void main(String[] args) {
-		Parser parser = new Parser();
-		parser.parse("clear");
+		//Parser parser = new Parser();
+		Terminal T = new Terminal();
+		//parser.parse("clear");
+		Scanner kb = new Scanner(System.in);
+		
+		
+		String comnd = kb.next();
+		if (comnd.equalsIgnoreCase("date")) {
+			T.date();
+		}
+		else if(comnd.equalsIgnoreCase("pwd")) {
+			T.pwd();
+		}
+		else if(comnd.equalsIgnoreCase("mkdir")) {
+			String filename =kb.next();
+			T.mkdir(filename);
+		}
+		else if(comnd.equalsIgnoreCase("ls")) {
+			T.ls();
+		}
+		
 	}
 	
 }
--- src/Terminal.java
+++ src/Terminal.java
@@ -1,4 +1,35 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Terminal {
 
+	public void date() {
+		Date date = new Date();
+		SimpleDateFormat aDate = new SimpleDateFormat("MM-dd-yyyy HH:mm:ss");
+		
+		System.out.println(aDate.format(date));
+		}
+	
+	public void pwd() {
+		String currentDir = System.getProperty("user.dir");
+		System.out.println(currentDir);
+	}
+	
+	public void mkdir(String dirName) {
+		boolean exists =  new File(dirName).mkdir();    
+	        if(!exists)
+	            System.out.println("a folder with that name already exists!");		
+		
+	}
+	
+	public void ls() {
+		
+	}
 }
