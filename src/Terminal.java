18275353a93d83896f5b79f302fa705c88c1a21e
y0ussefm0hamed <y0y01311999@gmail.com>
2019-10-11 18:11:24 +0200

    added functions to the terminal

--- src/Terminal.java
+++ src/Terminal.java
@@ -1,4 +1,67 @@
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 public class Terminal {
 
+	String defaultDir;
+	String path;
+
+	public Terminal() {
+		defaultDir = "C:/";
+		path = defaultDir;
+	}
+	
+	public void date() {
+		Date date = new Date();
+		SimpleDateFormat aDate = new SimpleDateFormat("MM-dd-yyyy HH:mm:ss");
+		
+		System.out.println(aDate.format(date));
+		}
+	
+	//public void pwd() {
+	//	String currentDir = System.getProperty("user.dir");
+	//	System.out.println(currentDir);
+	//}
+	
+	public void cd(String path) {
+		String backupPath = this.path;
+		if(path.equals("")) {
+			this.path = this.defaultDir;
+		}else if(path.charAt(1) == ':') {
+			this.path = path;
+		}else if(path.charAt(0) != '/') {
+			this.path = this.path + path;
+		}else if(path.charAt(0) == '/') {
+			this.path = "C:" + path;
+		}
+		
+		if(this.path.charAt(this.path.length()-1) != '/') {
+			this.path += "/";
+		}
+		
+		File f = new File(this.path);
+		if(!f.isDirectory()) {
+			System.out.println("invalid directory");
+			this.path = backupPath;
+		}
+	}
+	
+	
+	public void mkdir(String dirName) {
+		boolean exists =  new File(dirName).mkdir();    
+	        if(!exists)
+	            System.out.println("a folder with that name already exists!");		
+		
+	}
+	
+	public void ls() {
+		
+	}
+	
+
+	public void pwd() {
+		System.out.println(path);
+	}
 }
