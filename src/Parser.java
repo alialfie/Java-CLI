c7fad255f0e411a636b16e60f240aef24bc89b3f
y0ussefm0hamed <y0y01311999@gmail.com>
2019-10-19 07:30:41 +0200

    added args and operators and | more to cat and more

--- src/Parser.java
+++ src/Parser.java
@@ -15,12 +15,15 @@ public class Parser {
 				add(new Command("mv", 2, -1));
 				add(new Command("rmdir", 1, -1));
 				add(new Command("mkdir", 1, -1));
-				add(new Command("pwd", 0, 0));
-				add(new Command("date", 0, 0));
+				add(new Command("pwd", 0, 2));
+				add(new Command("date", 0, 2));
 				add(new Command("ls", 0, 2));
 				add(new Command("cat", 1, -1));
 				add(new Command("cp", 2, -1));
-				add(new Command("help", 0, 0));
+				add(new Command("rm", 1, 1));
+				add(new Command("args", 1, 1));
+				add(new Command("more",1, 2));
+				add(new Command("help", 0, 2));
 				add(new Command("exit", 0, 0));
 			}
 		};
@@ -31,7 +34,7 @@ public class Parser {
 		args = new ArrayList<String>();
 		
 		int length = input.length(), i = 0;
-		while(input.charAt(i) != ' ' && length != 0) {	//gets the command line
+		while(length != 0 && input.charAt(i) != ' ') {	//gets the command line
 			cmd += input.charAt(i);
 			i++;
 			if(i >= length) break;	//in case the index is out of range
@@ -50,6 +53,8 @@ public class Parser {
 			i++;	//skip the space
 		}
 		
+		transformArgs();
+		
 		return validateCommand();
 	}
 	
@@ -58,12 +63,40 @@ public class Parser {
 		Command current;
 		while(iterator.hasNext()) {
 			current = iterator.next();
-			if(current.getName().equals(cmd) && (current.getMinArgs() <= args.size()) &&
+			if(current.getName().equals(cmd) && (current.getMinArgs() <= args.size()) && 
 					((current.getMaxArgs() == -1) || (current.getMaxArgs() >= args.size())) ) {
 				return true;
 			}
 		}
 		return false;
+	}
+	
+	private void transformArgs() {		//handles the double quoted args
+		ArrayList<String> newArgs = new ArrayList<String>();
+		Iterator<String> iterator = args.iterator();
+		String arg, next;
+		while(iterator.hasNext()) {
+			arg = "";
+			arg = iterator.next();
+			if(arg.charAt(0) == '"') {
+				arg = arg.replace("\"", "");
+				while(iterator.hasNext()) {
+					next = iterator.next();
+					if(next.charAt(next.length()-1) == '"') {
+						next = next.replace("\"", "");
+						arg  = arg + " " + next;
+						break;
+					}else {
+						arg  = arg + " " + next;
+					}
+				}
+				newArgs.add(arg);
+			}else {
+				newArgs.add(arg);
+			}
+		}
+		
+		args = newArgs;
 	}
 
 	public String getCmd() {
--- src/Terminal.java
+++ src/Terminal.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -20,7 +21,7 @@ public class Terminal {
 		path = defaultDir;
 	}
 	
-	public void start() {
+	public void start() throws IOException {
 		Scanner sc = new Scanner(System.in);
 		Parser parser = new Parser();
 		String cmdLine;
@@ -40,7 +41,7 @@ public class Terminal {
 					break;
 					
 				case "pwd":
-					pwd();
+					pwd(parser.getArgs());
 					break;
 					
 				case "rmdir":
@@ -56,7 +57,7 @@ public class Terminal {
 					break;
 				
 				case "date":
-					date();
+					date(parser.getArgs());
 					break;
 				
 				case "cat":
@@ -64,7 +65,7 @@ public class Terminal {
 					break;
 				
 				case "help":
-					help();
+					help(parser.getArgs());
 					break;
 					
 				case "cp":
@@ -73,6 +74,19 @@ public class Terminal {
 				
 				case "mv":
 					mv(parser.getArgs());
+					break;
+
+				case "more":
+					more(parser.getArgs());
+					break;
+
+				case "args":
+					args(parser.getArgs());
+					break;
+
+					
+				case "rm":
+					rm(parser.getArgs());
 					break;
 					
 				case "exit":
@@ -101,16 +115,27 @@ public class Terminal {
 		if(this.path.charAt(this.path.length()-1) != '/') {
 			this.path += "/";
 		}
-		
-		File f = new File(this.path);
+				File f = new File(this.path);
 		if(!f.isDirectory()) {
 			System.out.println("invalid directory");
 			this.path = backupPath;
 		}
 	}
 	
-	public void pwd() {
-		System.out.println(path);
+	public void pwd(ArrayList<String> args) throws IOException {
+		if (args.isEmpty()) {
+			System.out.println(path);
+			return;
+		}
+		else if (args.get(0).compareTo(">>") == 0) {
+			FileWriter fileWriter = new FileWriter(args.get(1), true);
+			fileWriter.write(path);
+			fileWriter.close();
+		} else if(args.get(0).compareTo(">") == 0){
+			FileWriter fileWriter = new FileWriter(args.get(1), false);
+			fileWriter.write(path);
+			fileWriter.close();
+		}
 	}
 	
 	public void rmdir(ArrayList<String> args) {
@@ -198,12 +223,25 @@ public class Terminal {
 	
 ////////////////////////////////////////////////////////////////////////////////
 
-	public void date() {
+	public void date(ArrayList<String> args) throws IOException {
 		Date date = new Date();
 		SimpleDateFormat aDate = new SimpleDateFormat("MM-dd-yyyy HH:mm:ss");
-		
-		System.out.println(aDate.format(date));
+		if(args.size() == 0) {
+			System.out.println(aDate.format(date));
 		}
+		else if (args.get(0).compareTo(">>") == 0) {
+			FileWriter fileWriter = new FileWriter(args.get(1), true);
+			fileWriter.write(aDate.format(date));
+			fileWriter.close();
+			return;
+		} else if (args.get(0).compareTo(">") == 0) {
+			FileWriter fileWriter = new FileWriter(args.get(1), true);
+			fileWriter.write(aDate.format(date));
+			fileWriter.close();
+			return;
+		} 
+	}
+	
 	
 	public void mkdir(ArrayList<String> args) {
 		String currentPath;
@@ -223,7 +261,7 @@ public class Terminal {
 		
 	}
 
-	public void ls(ArrayList<String> args) {
+	public void ls(ArrayList<String> args) throws IOException {
 		if (args.size() == 0) {
 		String[] files;
 		File file = new File (path);
@@ -233,12 +271,36 @@ public class Terminal {
 			System.out.println(filenames);
 			}
 		}
+		else if (args.get(0).compareTo(">>") == 0) {
+			String[] files;
+			File file = new File(path);
+			files = file.list();
+			Arrays.sort(files, String.CASE_INSENSITIVE_ORDER);
+			FileWriter fileWriter = new FileWriter(args.get(1), true);
+			for (String filenames : files) {
+				fileWriter.write(filenames);
+				fileWriter.write("\n");
+			}
+			fileWriter.close();
+		}
+		// **************************************************************************************
+		else if (args.get(0).compareTo(">") == 0) {
+			String[] files;
+			File file = new File(path);
+			files = file.list();
+			Arrays.sort(files, String.CASE_INSENSITIVE_ORDER);
+			FileWriter fileWriter = new FileWriter(args.get(1), false);
+			for (String filenames : files) {
+				fileWriter.write(filenames);
+				fileWriter.write("\n");
+			}
+			fileWriter.close();
+		}
 		else if (args.size()==2 && args.get(0).equalsIgnoreCase("|") && args.get(1).equalsIgnoreCase("more")) {
 			Scanner kb = new Scanner (System.in);
 			int nOl = 0;
 			int sTop = 10;
 			String sMore;
-			String newline = System.getProperty("line.separator");
 			String[] files;
 			File file = new File (path);
 			files = file.list();
@@ -263,8 +325,59 @@ public class Terminal {
 	}
 	
 	
-	public void cat(ArrayList<String> args) {
+	public void cat(ArrayList<String> args) throws IOException {
+
 		String currentPath;
+		// ***********************************************************************************
+				if (args.size() > 2) {
+					if (args.get(1).compareTo(">>") == 0) {
+						Scanner freader = new Scanner(new File( args.get(0)));
+						FileWriter fileWriter = new FileWriter( args.get(2), true);
+						while (freader.hasNext()) {
+							String fdata = freader.next();
+							fileWriter.write(fdata);
+							fileWriter.write("\n");
+						}
+						freader.close();
+						fileWriter.close();
+						return;
+					}
+					if (args.get(1).compareTo(">") == 0) {
+						Scanner freader = new Scanner(new File( args.get(0)));
+						FileWriter fileWriter = new FileWriter( args.get(2), false);
+						while (freader.hasNext()) {
+							String fdata = freader.next();
+							fileWriter.write(fdata);
+							fileWriter.write("\n");
+							fileWriter.close();
+						}
+						freader.close();
+						fileWriter.close();
+						return;
+					}
+					if (args.get(1).compareTo("|") == 0 && args.get(2).compareTo("more") == 0) {
+						File fp = new File(args.get(0));int numOflines=1,j=0;
+						String Continue;Scanner sc = new Scanner(System.in);
+						Scanner freader = new Scanner(new File( args.get(0)));
+						FileWriter fileWriter = new FileWriter( args.get(2), false);
+						while (freader.hasNext()) {
+							String fdata = freader.next();
+							System.out.println(fdata);
+							j++;
+							if (j == numOflines * 10) {
+								System.out.println("if you want countinue press y");
+								Continue = sc.nextLine();
+								if (Continue.matches("y")) {
+									numOflines++;
+								} else {
+									break;
+								}
+							}
+						}
+						freader.close();
+						return;
+					}
+				}
 		for (int i=0; i<args.size(); i++) {
 		try {
 			File fp = new File (args.get(i));
@@ -285,20 +398,21 @@ public class Terminal {
 		}
 		}
 	}
+
 	
 	
 	public void cp(ArrayList<String> args) {
 		String currentPath;
 		//could be changed to copy any number of files into a directory
-		if (args.size() == 3) {
+		if (args.size() >= 3) {
 			//checking if the last argument is a directory.
-			File fp = new File (args.get(2));
+			File fp = new File (args.get(args.size()-1));
 			
-			if (!args.get(2).equalsIgnoreCase(fp.getAbsolutePath())) {
-			currentPath = this.path + args.get(2);
+			if (!args.get(args.size()-1).equalsIgnoreCase(fp.getAbsolutePath())) {
+			currentPath = this.path + args.get(args.size()-1);
 			}
 			else {
-				currentPath = args.get(2);
+				currentPath = args.get(args.size()-1);
 			}
 			
 			fp = new File(currentPath);
@@ -308,9 +422,9 @@ public class Terminal {
 			}
 			
 			else {	
-				for(int i=0 ; i<2; i++) {
+				for(int i=0 ; i<args.size()-1; i++) {
 					//if else for long and short paths
-					if (!args.get(2).equalsIgnoreCase(fp.getAbsolutePath())) {
+					if (!args.get(args.size()-1).equalsIgnoreCase(fp.getAbsolutePath())) {
 						currentPath = this.path + args.get(i);
 						}
 						else {
@@ -368,21 +482,147 @@ public class Terminal {
 		
 	}
 	
-	public void help() {
-		System.out.println("cd: This command changes the current directory to another one. arguments: directoryname");
-		System.out.println("ls: list names of all files and directories in the current working directory");
-		System.out.println("cp: copies a file's contents into a new file that has been created by it or copies files into a certain directory. arguments: filename filename or filename filename directoryname");
-		System.out.println("cat: concatenates files and outputs their content to the console/anotherfile. arguments: filename(s)");
-		System.out.println("more: lets us display the output line by line or page by page. arguments: filename");
-		System.out.println("mkdir: makes directories of given names in current directory or in a specified path. arguments: directoryname(s) or path(s)/dirname(s)");
-		System.out.println("rmdir: removes directories of given names in current directory or in a specified path. arguments: directoryname(s) or path(s)/dirname(s)");
-		System.out.println("mv: moves each given file into a the last given file with the same name in a specified directory. arguments: filenames/paths");
-		System.out.println("rm: removes each specified file. arrguments: filename(s)");
-		System.out.println("args: list all command arguments");
-		System.out.println("date: current date/time");
-		System.out.println("pwd: shows current working directory");
-		System.out.println("clear: clears the current terminal screen ");
-		System.out.println("exit: Stop all");
+	
+	public void help(ArrayList<String> args) throws IOException {
+		String helpMessage = "cd: This command changes the current directory to another one. arguments: directoryname \n"
+				+ " ls: list names of all files and directories in the current working directory\n"
+				+ "cp: copies a file's contents into a new file that has been created by it or copies files into a certain directory. arguments: filename filename or filename filename directoryname"
+				+ "\ncat: concatenates files and outputs their content to the console/anotherfile. arguments: filename(s)"
+				+ "\nmore: lets us display the output line by line or page by page. arguments: filename"
+				+ "\nmkdir: makes directories of given names in current directory or in a specified path. arguments: directoryname(s) or path(s)/dirname(s)"
+				+ "\nrmdir: removes directories of given names in current directory or in a specified path. arguments: directoryname(s) or path(s)/dirname(s)"
+				+ "\nmv: moves each given file into a the last given file with the same name in a specified directory. arguments: filenames/paths"
+				+ "\nrm: removes each specified file. arrguments: filename(s)" + "\nargs: list all command arguments"
+				+ "\ndate: current date/time" + "\npwd: shows current working directory"
+				+ "\nclear: clears the current terminal screen " + "\nexit: Stop all";
+		if (args.isEmpty()) {
+			System.out.println(helpMessage);
+			return;
+		} else if (args.get(0).compareTo(">>") == 0) {
+			FileWriter fileWriter = new FileWriter(args.get(1), true);
+			fileWriter.write(helpMessage);
+			fileWriter.close();
+			return;
+		} else if (args.get(0).compareTo(">") == 0) {
+			FileWriter fileWriter = new FileWriter(args.get(1), false);
+			fileWriter.write(helpMessage);
+			fileWriter.close();
+			return;
+		}
+
 	}
+
+	public void rm(ArrayList<String> args) {
+		File file = new File( args.get(0));
+
+		if (!file.delete()) {
+			System.out.println(args.get(0) + " does not exist");
+		}
+
+	}
+	
+	public void more(ArrayList<String> args) throws IOException {
+		int numOflines = 1;
+		String Continue;
+		Scanner sc = new Scanner(System.in);
+		if (args.get(0).compareTo("ls") == 0) {
+			String[] files;
+			File file = new File(path);
+			files = file.list();
+			Arrays.sort(files, String.CASE_INSENSITIVE_ORDER);
+
+			for (int i = 0; i < files.length; i++) {
+				String filenames = files[i];
+				System.out.println(filenames);
+				if (i == numOflines * 10) {
+					System.out.println("if you want countinue press y");
+					Continue = sc.nextLine();
+					if (Continue.matches("y")) {
+						numOflines++;
+					} else {
+						break;
+					}
+
+				}
+			}
+
+		} else if (args.get(0).compareTo("cat") == 0) {
+			String currentPath;
+			for (int i = 0; i < args.size(); i++) {
+				try {
+					File fp = new File(args.get(i));
+					if (!args.get(i).equalsIgnoreCase(fp.getAbsolutePath())) {
+						currentPath = this.path + args.get(i);
+					} else {
+						currentPath = args.get(i);
+					}
+					Scanner freader = new Scanner(new File(currentPath));
+					int j = 0;
+					while (freader.hasNext()) {
+						String fdata = freader.next();
+						System.out.println(fdata);
+						j++;
+						if (j == numOflines * 10) {
+							System.out.println("if you want countinue press y");
+							Continue = sc.nextLine();
+							if (Continue.matches("y")) {
+								numOflines++;
+							} else {
+								break;
+							}
+						}
+					}
+					freader.close();
+				} catch (FileNotFoundException e) {
+					System.out.println("file not found");
+				}
+			}
+		}
+	}
+	
+	
+	public void args(ArrayList<String> args) {
+		switch(args.get(0)) {
+		case "cp":
+			System.out.println("sourcePath destinationPath");
+			break;
+			
+		case "cd":
+			System.out.println("[path]");
+			break;
+			
+		case "ls":
+			System.out.println("no arguments");
+			break;
+			
+		case "cat":
+			System.out.println("file [file..]");
+			break;
+			
+		case "more":
+			System.out.println("no argments");
+			break;
+			
+		case "mkdir":
+			System.out.println("directory [directory ...]");
+			break;
+			
+		case "rmdir":
+			System.out.println("directory ...");
+			break;
+			
+		case "mv":
+			System.out.println("source dest");
+			break;
+			
+		case "rm":
+			System.out.println("file ...");
+			break;
+			
+		default:
+			System.out.println("unknown command");
+		}
+	}
+
 	
 } 
